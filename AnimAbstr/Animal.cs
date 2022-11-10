280a5fe788338787ba077b8967283e2e2efb269b
LarisaChAng <LChS81@mail.ru>
2022-11-10 13:51:52 +0800

    Добавьте файлы проекта.

---
+++ AnimAbstr/Animal.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimAbstr
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+    }
+    class Cat : Animal
+    {
+        private string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Cat(string name)
+            : base(name)
+        {
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine("Мяу!");
+        }
+    }
+    class Dog : Animal
+    {
+        int age;
+        private string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Dog(string name, int age)
+            : base(name)
+        {
+            this.age = age;
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine("Гав! Мой возраст - {0}", age);
+        }
+    }
+}
---
+++ AnimAbstr/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimAbstr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Мурка");
+            Dog dog = new Dog("Лайка", 5);
+            cat.ShowInfo();
+            dog.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
